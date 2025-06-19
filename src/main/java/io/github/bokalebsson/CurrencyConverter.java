e74f3962a7686d977479f4c28e96d775ab7ab3f1
BoKalebsson <bo_kalebsson@hotmail.com>
2025-06-19 11:28:05 +0200

    Created the CurrencyConverter Class with several exchange rate conversion methods.

---
+++ src/main/java/io/github/bokalebsson/CurrencyConverter.java
@@ -0,0 +1,28 @@
+package io.github.bokalebsson;
+
+public class CurrencyConverter {
+
+    // Exchange rates. How much one unit of USD or EUR costs in SEK.
+    private static final double USD_TO_SEK_RATE = 10.75;
+    private static final double EURO_TO_SEK_RATE = 11.36;
+
+    // Converts SEK to USD.
+    public static double convertSekToUsd(double sek) {
+        return sek / USD_TO_SEK_RATE;
+    }
+
+    // Converts SEK to EURO.
+    public static double convertSekToEuro(double sek) {
+        return sek / EURO_TO_SEK_RATE;
+    }
+
+    // Converts USD to SEK.
+    public static double convertUsdToSek(double usd) {
+        return usd * USD_TO_SEK_RATE;
+    }
+
+    // Converts EURO to SEK.
+    public static double convertEuroToSek(double euro) {
+        return euro * EURO_TO_SEK_RATE;
+    }
+}
